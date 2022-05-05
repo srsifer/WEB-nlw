4d74c435980704b76b5191203edecd57b37c7c3c
igor s fernandes <igorsife80@gmail.com>
2022-05-05 13:51:18 -0300

    feat: cria o component feedback content step (conteudo do feedback a ser enviado)

---
+++ src/components/widgetForms/steps/feedBackContentStep.tsx
@@ -0,0 +1,78 @@
+import { ArrowLeft, Camera } from 'phosphor-react';
+import React, { FormEvent, useState } from 'react'
+import { FeedBackType, feedbackTypes } from '..'
+import { CloseButton } from '../../CloseButton'
+import { ScreenshotButton } from '../ScreenshotButton';
+
+interface FeedBackContentStepProps {
+  feedBackType: FeedBackType
+  onFeedBackRestartedRequested: () => void;
+  onFeedbackSent: () => void;
+}
+
+export function FeedBackContentStep({
+  feedBackType,
+  onFeedBackRestartedRequested,
+  onFeedbackSent,
+}: FeedBackContentStepProps) {
+  const [ screenShot, setScreenShot] = useState<String | null>(null)
+  const [comment , setComment] = useState('')
+  const feedBackTypeInfo = feedbackTypes[feedBackType];
+
+  const handleSubmitFeedBack = (event: FormEvent) => {
+    event.preventDefault();
+
+    console.log({
+      screenShot,
+      comment,
+    })
+
+    onFeedbackSent()
+  }
+
+
+  return (
+    <>
+      <header>
+
+        <button
+          type='button'
+          className='top-5 left-5 absolute text-zinc-400 hover:text-zinc-100'
+          onClick={onFeedBackRestartedRequested}
+        >
+
+          <ArrowLeft weight='bold' className='w-4 h-4' />
+        </button>
+
+        <span className=" text-xl leading-6 flex items-center gap-2">
+          <img src={feedBackTypeInfo.image.source} alt={feedBackTypeInfo.image.alt} className="w-6 h-6" />
+          {feedBackTypeInfo.title}
+        </span>
+
+        <CloseButton />
+      </header>
+      <div className="flex py-8 gap-2 w-full">
+        <form onSubmit={handleSubmitFeedBack} className="my-4 w-full">
+          <textarea
+            className="min-w-[304px] w-full min-h-[112px] text-sm placeholder:text-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
+            placeholder="Conte com detalhes oque esta acontecendo..."
+            onChange={e => setComment(e.target.value)}
+          />
+          <footer className="flex gap-2 mt-2">
+            <ScreenshotButton 
+              onScreenshoTook={setScreenShot}
+              screenShot={screenShot}
+            />
+            <button
+              disabled={comment.length === 0}
+              type="submit"
+              className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
+            >
+              Enviar feedback
+            </button>
+          </footer>
+        </form>
+      </div>
+    </>
+  )
+}
